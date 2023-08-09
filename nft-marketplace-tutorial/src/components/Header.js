3e8f7a4e9fe0a5b26d9c131f1d4a4915759d2d00
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-08-09 15:16:33 -0700

    header component with logo

---
+++ nft-marketplace-tutorial/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+import punkLogo from '../assets/header/cryptopunk-logo.png';
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className='logoContainer'>
+        <img src={punkLogo} alt='logo' className='punkLogo' />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
