8f232c4e1375e449ccfa0ae9ad78b151216f47c6
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-08-09 15:42:19 -0700

    search container

--- nft-marketplace-tutorial/src/components/Header.js
+++ nft-marketplace-tutorial/src/components/Header.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './Header.css';
 import punkLogo from '../assets/header/cryptopunk-logo.png';
+import searchIcon from '../assets/header/search.png';
 
 const Header = () => {
   return (
     <div className='header'>
       <div className='logoContainer'>
         <img src={punkLogo} alt='logo' className='punkLogo' />
+      </div>
+      <div className='searchContainer'>
+        <div className='searchIcon'>
+          <img src={searchIcon} alt='searchIcon' />
+        </div>
+        <input
+          className='searchInput'
+          placeholder='Collection, User or Item...'
+        />
       </div>
     </div>
   );
