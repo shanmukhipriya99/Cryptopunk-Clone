a66f2ea5209f3692bd737ef6274d400f24e84923
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-08-09 18:06:32 -0700

    punklist

--- nft-marketplace-tutorial/src/App.js
+++ nft-marketplace-tutorial/src/App.js
@@ -3,13 +3,14 @@ import CollectionCard from './components/CollectionCard';
 import Header from './components/Header';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import PunkList from './components/PunkList';
 
 function App() {
   const [punkListData, setPunkListData] = useState([]);
 
   const getMyNFTs = async () => {
     const openSeaData = await axios.get(
-      'https://testnets-api.opensea.io/assets?asset_contract_address=0x653D5eea31667C9f97c0B08C99ebF4c8f5715c1D&order_direction=asc'
+      'https://testnets-api.opensea.io/api/v1/assets?asset_contract_address=0xb1bb5732D99C458222302faCB4cD7F925d79A073&order_direction=asc&offset=0'
     );
     setPunkListData(openSeaData.data.assets);
   };
@@ -21,12 +22,7 @@ function App() {
   return (
     <div className='app'>
       <Header />
-      <CollectionCard
-        id={0}
-        name={'Bandana Punk'}
-        traits={[{ value: 8 }]}
-        image='https://i.seadn.io/gcs/files/2d0b5e43da4cce75eb525e4509fa6ec1.jpg?auto=format&dpr=1&w=1000'
-      />
+      <PunkList punkListData={punkListData} />
     </div>
   );
 }
